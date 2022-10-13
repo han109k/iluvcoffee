d6370ae06be0838a2a0345ce2818f2815e51cd3a
Han <han109k@gmail.com>
2022-10-13 22:22:49 +0300

    service, dto, mapped types, validation pipe properties

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -9,51 +9,38 @@ import {
   Patch,
   Post,
   Query,
-  Res,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
-  // @Get()
-  // findAll(@Res() response) {
-  //   // Express response object
-  //   response.status(200).send('This aciton returns all coffees');
-  // }
-
   @Get()
   findAll(@Query() paginationQuery) {
-    const { limit, offset } = paginationQuery;
-    return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`;
+    // const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param() params) {
-    // Param() lets us use HTTP parameters in nestJS app
-    console.log(params);
-    return `This action returns #${params.id} coffee`;
-  }
-
-  @Get('/another/:id')
-  findAnotherOne(@Param('id') id: string) {
-    return `This action returns #${id} coffee`;
+  findOne(@Param('id') id: string) {
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
-  @HttpCode(HttpStatus.GONE) // customizing HTTP response code
-  create(@Body() body) {
-    return body;
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    return this.coffeesService.create(createCoffeeDto);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    return `This action updates #${id} coffee`;
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, updateCoffeeDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This action removes #${id} coffee`;
+    return this.coffeesService.remove(id);
   }
 }
